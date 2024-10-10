b616449294c0b4993d9d67cee06eebcee493f209
Puvanat.T <puvanat.t@bainisys.com>
2024-10-10 12:40:02 +0700

    initial

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const port = 3000
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res){
+   console.log(req.body);
+   res.send('OK');
+})
+
+app.listen(port)
+console.log("Server started on port " + port)
